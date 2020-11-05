8f43dcff6c759caf9d1eb78431578db143eece78
Aflah Sedhique <aflah1211@outlook.com>
2020-11-05 18:12:37 +0530

    ALL COMPLETED. ONLY UTILITY BUTTONS TO DO

--- BITMAP RESTAURANT PROJECT.py
+++ BITMAP RESTAURANT PROJECT.py
@@ -13,7 +13,7 @@ root.configure(bg="white")
 
 #*******CREATING FRAMES*******************
 
-top_frame=Frame(root,width=1600,height=50,bg='#B2BABB',relief='sunken')
+top_frame=Frame(root,width=1600,height=50,bg='#48C9B0',relief='sunken')
 top_frame.pack(side=TOP)
 
 left_frame=Frame(root,width=800,height=700,relief='sunken')
@@ -27,11 +27,14 @@ right_frame.pack(side=RIGHT)
 #**********CREATING LABEL INFORMATION**************
 
 
-label_info=Label(top_frame,text='BITMAP HOTEL MANAGEMENT',bd=10,font=('courier new',50,'bold'),anchor='w',fg='#16A085')
+label_info=Label(top_frame,text='BITMAP HOTEL MANAGEMENT',bd=10,font=('courier new',50,'bold'),anchor='w',fg='#229954')
 label_info.grid(row=0,column=0)
 
 #*****************************MAIN DISPLAY INFORMATION******************
 
+mainequation=StringVar()
+main_display=Entry(top_frame,textvariable=mainequation,justify='right',width=55,bg='#48C9B0',bd=15,fg='#17202A',font=('arial',20,'bold'))
+main_display.grid(row=2,column=0)
 
 
 
@@ -68,6 +71,21 @@ def clearpress():
     equation.set('0')
     expression=""
 
+
+def pressoff():
+    global expression
+    mainequation.set(" POWERED OFF!!! ")
+    expression=""
+
+def presscloss():
+    global expression
+    mainequation.set("SHOP CLOSED! THANK YOU!!!!!!!!")
+    expression=""
+
+def presscash():
+    global expression
+    mainequation.set("CASHIER OPENED AND RECORDED")
+    expression=""
 
 
 
@@ -138,50 +156,69 @@ multiply.grid(row=3,column=3)
 divide=Button(right_frame,text='/',font=('arial',20,'bold'),bg='#48C9B0',bd=8,padx=16,pady=16,fg='black',command=lambda:press('/'))
 divide.grid(row=4,column=3)
 
+#CREATE OTHER FUNCTION BUTTONS
+
+off_button=Button(right_frame,text='Off!',font=('verdana',12,'bold'),bg='#48C9B0',bd=8,padx=10,pady=19,fg='black',command=lambda:pressoff())
+off_button.grid(row=5,column=1)
+
+close_button=Button(right_frame,text='Close',font=('verdana',12,'bold'),bg='#48C9B0',bd=8,padx=10,pady=19,fg='black',command=lambda:presscloss())
+close_button.grid(row=5,column=2)
+
+cashier_button=Button(right_frame,text='Cash',font=('verdana',12,'bold'),bg='#48C9B0',bd=8,padx=10,pady=19,fg='black',command=lambda:presscash())
+cashier_button.grid(row=5,column=3)
+
+
+
+
+
+
+
+
+
 
 #***********RESTAURANT INFORMATION(1) FOR FFRAME(1)***************************
 
 
-label_refer=Label(left_frame,text='Reference',font=('verdana',16,'bold'),bd=16,anchor='w',)
+label_refer=Label(left_frame,text='Reference',font=('arial',16,'bold'),bd=16,anchor='w',)
 label_refer.grid(row=0,column=0)
 
 disp=StringVar()
-text_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+text_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 text_refr.grid(row=0,column=1)
 
-meal1_refer=Label(left_frame,text='Meal 1: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+meal1_refer=Label(left_frame,text='Meal 1: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 meal1_refer.grid(row=1,column=0)
 
 disp1=StringVar()
-textmeal1_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+textmeal1_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 textmeal1_refr.grid(row=1,column=1)
 
-meal2_refer=Label(left_frame,text='Meal 2: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+meal2_refer=Label(left_frame,text='Meal 2: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 meal2_refer.grid(row=2,column=0)
 
 disp2=StringVar()
-textmeal2_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+textmeal2_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 textmeal2_refr.grid(row=2,column=1)
 
-meal3_refer=Label(left_frame,text='Meal 3: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+meal3_refer=Label(left_frame,text='Meal 3: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 meal3_refer.grid(row=3,column=0)
 
 disp3=StringVar()
-textmeal3_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+textmeal3_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 textmeal3_refr.grid(row=3,column=1)
 
-meal4_refer=Label(left_frame,text='Meal 4: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+meal4_refer=Label(left_frame,text='Meal 4: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 meal4_refer.grid(row=4,column=0)
 
 disp4=StringVar()
-textmeal4_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+textmeal4_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 textmeal4_refr.grid(row=4,column=1)
 
-meal5_refer=Label(left_frame,text='Meal 5: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+meal5_refer=Label(left_frame,text='Meal 5: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 meal5_refer.grid(row=5,column=0)
 
 disp5=StringVar()
-textmeal5_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+textmeal5_refr=Entry(left_frame,font=('ariall',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 textmeal5_refr.grid(row=5,column=1)
 
 
@@ -189,48 +226,53 @@ textmeal5_refr.grid(row=5,column=1)
 #**********RESTAURANT INFORMATION(2) FOR FRAME(1)***********************************************
 
 
-drinks_refer=Label(left_frame,text='DRINKS',font=('verdana',16,'bold'),bd=16,anchor='w',)
+drinks_refer=Label(left_frame,text='DRINKS',font=('arial',16,'bold'),bd=16,anchor='w',)
 drinks_refer.grid(row=0,column=2)
 
 disp7=StringVar()
-drinks_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+drinks_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 drinks_refr.grid(row=0,column=3)
 
-mealcost_refer=Label(left_frame,text='Meal Cost (T)',font=('verdana',16,'bold'),bd=16,anchor='w',)
+mealcost_refer=Label(left_frame,text='Meal Cost (T)',font=('arial',16,'bold'),bd=16,anchor='w',)
 mealcost_refer.grid(row=1,column=2)
 
 disp8=StringVar()
-mealcost_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+mealcost_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 mealcost_refr.grid(row=1,column=3)
 
 
-servcharge_refer=Label(left_frame,text='Service Charge: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+servcharge_refer=Label(left_frame,text='Service Charge: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 servcharge_refer.grid(row=2,column=2)
 
 disp9=StringVar()
-servcharge_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+servcharge_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 servcharge_refr.grid(row=2,column=3)
 
-tax_refer=Label(left_frame,text='State Tax: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+tax_refer=Label(left_frame,text='State Tax: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 tax_refer.grid(row=3,column=2)
 
 disp10=StringVar()
-tax_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+tax_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 tax_refr.grid(row=3,column=3)
 
-subt_refer=Label(left_frame,text='Sub Total: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
+subt_refer=Label(left_frame,text='Sub Total: ',font=('arial',16,'bold'),bd=16,anchor='w',)
 subt_refer.grid(row=4,column=2)
 
 disp11=StringVar()
-subt_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+subt_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 subt_refr.grid(row=4,column=3)
 
 total_refer=Label(left_frame,text='Total Cost: ',font=('verdana',16,'bold'),bd=16,anchor='w',)
 total_refer.grid(row=5,column=2)
 
 disp12=StringVar()
-total_refr=Entry(left_frame,font=('verdana',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
+total_refr=Entry(left_frame,font=('arial',16,'bold'),bd=16,textvariable=disp,insertwidth=4,bg='#16A085',justify='right')
 total_refr.grid(row=5,column=3)
+
+
+#*********UTILITY BUTTONS*****************************
+
+
 
 
 
