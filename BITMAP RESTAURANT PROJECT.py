b523a3ab1987c63d2358115ab182afa96a20ceb1
Aflah Sedhique <aflah1211@outlook.com>
2020-11-05 09:01:04 +0530

    Base Layer for Restaurant Project Has Been Created

---
+++ BITMAP RESTAURANT PROJECT.py
@@ -0,0 +1,14 @@
+#****************IMPORT SECTIONS***********************8
+from tkinter import *
+import random
+
+
+
+#*********CREATING WINDOW FOR THE RESTAURANT**************
+
+root=Tk()
+root.geometry('1600x800+0+0')
+root.title('BITMAP RESTAUTRANT PLAN ')
+root.configure(bg="white")
+root.mainloop()
+
